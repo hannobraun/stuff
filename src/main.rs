887669b76939f0d08795d486b1ea4a4677632ac5
Hanno Braun <hanno@braun-odw.eu>
2022-12-16 21:38:43 +0100

    Add documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//! Interaction Nets Experiment
+//!
+//! This is my attempt to understand [interaction nets][paper]. As usual, I'm
+//! having trouble understanding the paper itself. I found the [diploma thesis]
+//! by Simon Gay very helpful.
+//!
+//! [paper]: https://dl.acm.org/doi/pdf/10.1145/96709.96718
+//! [diploma thesis]: https://www.dcs.gla.ac.uk/~simon/publications/diploma.pdf
+
 fn main() {
     println!("Hello, world!");
 }

64d40d4a4e9bbb06f47949e295259d163ba543ec
Hanno Braun <hanno@braun-odw.eu>
2022-12-18 10:06:51 +0100

    Add Window

--- orbital/src/event_loop.rs
+++ orbital/src/event_loop.rs
@@ -1,9 +1,13 @@
-use winit::event_loop::EventLoop;
+use winit::{event::Event, event_loop::EventLoop, window::Window};
 
 pub fn run() -> anyhow::Result<()> {
     let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop)?;
 
-    event_loop.run(|event, _, _| match event {
+    event_loop.run(move |event, _, _| match event {
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
         _ => {
             dbg!(event);
         }

f71d61f464bb79e5692626f5dc3f60922341f631
Hanno Braun <hanno@braun-odw.eu>
2022-12-18 10:03:18 +0100

    Refactor

--- orbital/src/event_loop.rs
+++ orbital/src/event_loop.rs
@@ -3,7 +3,9 @@ use winit::event_loop::EventLoop;
 pub fn run() {
     let event_loop = EventLoop::new();
 
-    event_loop.run(|event, _, _| {
-        dbg!(event);
+    event_loop.run(|event, _, _| match event {
+        _ => {
+            dbg!(event);
+        }
     })
 }

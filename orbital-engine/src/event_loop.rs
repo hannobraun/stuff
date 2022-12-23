bbac3f0eafbfbd9e76e706ecc0e2ef90acb76ba9
Hanno Braun <hanno@braun-odw.eu>
2022-12-23 16:00:29 +0100

    Add `Host::new`

--- orbital-engine/src/event_loop.rs
+++ orbital-engine/src/event_loop.rs
@@ -1,5 +1,3 @@
-use anyhow::bail;
-use tokio::{fs::File, io::AsyncReadExt, process::Command};
 use winit::{
     event::{Event, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -14,30 +12,7 @@ pub async fn run() -> anyhow::Result<()> {
         .with_maximized(true)
         .build(&event_loop)?;
     let mut renderer = Renderer::new(&window).await?;
-
-    let status = Command::new("cargo")
-        .arg("rustc")
-        .args(["--manifest-path", "../orbital-game/Cargo.toml"])
-        .args(["--target", "wasm32-unknown-unknown"])
-        .args(["--crate-type", "cdylib"])
-        .status()
-        .await?;
-    if !status.success() {
-        bail!("Error building WASM module");
-    }
-
-    let mut wasm = Vec::new();
-    File::open("../target/wasm32-unknown-unknown/debug/orbital_game.wasm")
-        .await?
-        .read_to_end(&mut wasm)
-        .await?;
-
-    let mut store = wasmer::Store::default();
-    let module = wasmer::Module::new(&store, &wasm)?;
-    let imports = wasmer::imports! {};
-    let instance = wasmer::Instance::new(&mut store, &module, &imports)?;
-
-    let mut host = Host { store, instance };
+    let mut host = Host::new().await?;
 
     let mut color = [0., 0., 0., 1.];
 
--- orbital-engine/src/host.rs
+++ orbital-engine/src/host.rs
@@ -1,4 +1,35 @@
+use anyhow::bail;
+use tokio::{fs::File, io::AsyncReadExt, process::Command};
+
 pub struct Host {
     pub store: wasmer::Store,
     pub instance: wasmer::Instance,
+}
+
+impl Host {
+    pub async fn new() -> anyhow::Result<Self> {
+        let status = Command::new("cargo")
+            .arg("rustc")
+            .args(["--manifest-path", "../orbital-game/Cargo.toml"])
+            .args(["--target", "wasm32-unknown-unknown"])
+            .args(["--crate-type", "cdylib"])
+            .status()
+            .await?;
+        if !status.success() {
+            bail!("Error building WASM module");
+        }
+
+        let mut wasm = Vec::new();
+        File::open("../target/wasm32-unknown-unknown/debug/orbital_game.wasm")
+            .await?
+            .read_to_end(&mut wasm)
+            .await?;
+
+        let mut store = wasmer::Store::default();
+        let module = wasmer::Module::new(&store, &wasm)?;
+        let imports = wasmer::imports! {};
+        let instance = wasmer::Instance::new(&mut store, &module, &imports)?;
+
+        Ok(Self { store, instance })
+    }
 }

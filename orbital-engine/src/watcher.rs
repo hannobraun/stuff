7283b75ace0858e4be6b0b3fa1fbf3866b2292de
Hanno Braun <hanno@braun-odw.eu>
2022-12-24 13:11:10 +0100

    Add `Watcher`

--- orbital-engine/src/watcher.rs
+++ orbital-engine/src/watcher.rs
@@ -1,11 +1,15 @@
 use std::path::Path;
 
-use notify::{RecursiveMode, Watcher};
+use notify::{RecursiveMode, Watcher as _};
 
-pub fn start() -> anyhow::Result<notify::RecommendedWatcher> {
+pub struct Watcher {
+    _watcher: notify::RecommendedWatcher,
+}
+
+pub fn start() -> anyhow::Result<Watcher> {
     let mut watcher = notify::recommended_watcher(|event| {
         println!("Game code changed: {event:?}");
     })?;
     watcher.watch(Path::new("../orbital-game"), RecursiveMode::Recursive)?;
-    Ok(watcher)
+    Ok(Watcher { _watcher: watcher })
 }

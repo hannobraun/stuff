e09608eb8124bbc608a8eb744997d1cb18d89689
Hanno Braun <hanno@braun-odw.eu>
2022-12-25 16:01:12 +0100

    Refactor

--- orbital-engine/src/host.rs
+++ orbital-engine/src/host.rs
@@ -8,28 +8,7 @@ pub struct Host {
 
 impl Host {
     pub async fn new() -> anyhow::Result<Self> {
-        let status = Command::new("cargo")
-            .arg("rustc")
-            .args(["--manifest-path", "../orbital-game/Cargo.toml"])
-            .args(["--target", "wasm32-unknown-unknown"])
-            .args(["--crate-type", "cdylib"])
-            .status()
-            .await?;
-        if !status.success() {
-            bail!("Error building WASM module");
-        }
-
-        let mut wasm = Vec::new();
-        File::open("../target/wasm32-unknown-unknown/debug/orbital_game.wasm")
-            .await?
-            .read_to_end(&mut wasm)
-            .await?;
-
-        let mut store = wasmer::Store::default();
-        let module = wasmer::Module::new(&store, &wasm)?;
-        let imports = wasmer::imports! {};
-        let instance = wasmer::Instance::new(&mut store, &module, &imports)?;
-
+        let (store, instance) = compile_and_load().await?;
         Ok(Self { store, instance })
     }
 
@@ -42,4 +21,31 @@ impl Host {
 
         Ok([value, value, value, 1.])
     }
+}
+
+async fn compile_and_load() -> anyhow::Result<(wasmer::Store, wasmer::Instance)>
+{
+    let status = Command::new("cargo")
+        .arg("rustc")
+        .args(["--manifest-path", "../orbital-game/Cargo.toml"])
+        .args(["--target", "wasm32-unknown-unknown"])
+        .args(["--crate-type", "cdylib"])
+        .status()
+        .await?;
+    if !status.success() {
+        bail!("Error building WASM module");
+    }
+
+    let mut wasm = Vec::new();
+    File::open("../target/wasm32-unknown-unknown/debug/orbital_game.wasm")
+        .await?
+        .read_to_end(&mut wasm)
+        .await?;
+
+    let mut store = wasmer::Store::default();
+    let module = wasmer::Module::new(&store, &wasm)?;
+    let imports = wasmer::imports! {};
+    let instance = wasmer::Instance::new(&mut store, &module, &imports)?;
+
+    Ok((store, instance))
 }

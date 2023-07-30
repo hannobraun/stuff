125860f428cd2e34c703c63fe8341fd9fa583247
Hanno Braun <hanno@braun-odw.eu>
2023-07-30 18:32:25 +0200

    Implement simple oscillator

--- synth/src/main.rs
+++ synth/src/main.rs
@@ -1,4 +1,32 @@
+use std::{thread::sleep, time::Duration};
+
+use anyhow::anyhow;
+use tinyaudio::{run_output_device, OutputDeviceParameters};
+
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let params = OutputDeviceParameters {
+        sample_rate: 48000,
+        channels_count: 1,
+        channel_sample_count: 48000,
+    };
+
+    let mut t = 0.;
+
+    let _device = run_output_device(params, move |data| {
+        for value in data {
+            t += 440. / params.sample_rate as f32;
+            t %= 1.;
+
+            if t < 0.5 {
+                *value = 0.;
+            } else {
+                *value = 0.1;
+            }
+        }
+    })
+    .map_err(|err| anyhow!("{}", err))?;
+
+    sleep(Duration::from_secs(5));
+
     Ok(())
 }

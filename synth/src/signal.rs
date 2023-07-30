615de7c2553af3c360a6945b11cd82205fbef68b
Hanno Braun <hanno@braun-odw.eu>
2023-07-30 21:16:11 +0200

    Remove outdated comment

--- synth/src/signal.rs
+++ synth/src/signal.rs
@@ -1,13 +1,6 @@
 use crate::clock::Clock;
 
 pub trait Signal: Send {
-    // The way this is currently used, `next_value` is called and that moves the
-    // signal forward. This is not good, once the same signal can be used as
-    // input in multiple places. We don't want multiple calls to this to advance
-    // the signal multiple times!
-    //
-    // Probably best to make this take `&self` and a global clock value. Then
-    // everything is pure, and we don't have to worry about stuff like that.
     fn next_value(&mut self, clock: &Clock) -> f32;
 }
 

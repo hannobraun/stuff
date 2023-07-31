1c070eb9916897b8e7fbadb26f7c56cc0134c9f9
Hanno Braun <hanno@braun-odw.eu>
2023-07-31 21:16:02 +0200

    Use winit to handle input

--- synth/src/ui.rs
+++ synth/src/ui.rs
@@ -1,58 +1,75 @@
+use std::num::NonZeroU32;
+
 use crossbeam_channel::{SendError, Sender};
-use crossterm::event::{self, Event, KeyCode, KeyModifiers};
+use winit::{
+    event::{Event, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
 
 use crate::synth::input::{Input, Note};
 
 pub fn run(input: Sender<Input>) {
-    loop {
-        let event = {
-            match event::read().unwrap() {
-                Event::Key(key) => match key.code {
-                    KeyCode::Char('c') => {
-                        if key.modifiers.contains(KeyModifiers::CONTROL) {
-                            break;
-                        } else {
-                            continue;
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
+    let mut surface =
+        unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        let event = match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+                WindowEvent::KeyboardInput { input, .. } => {
+                    match input.virtual_keycode {
+                        Some(VirtualKeyCode::Escape) => {
+                            *control_flow = ControlFlow::Exit;
+                            return;
                         }
+
+                        Some(VirtualKeyCode::Left) => Input::OctaveDec,
+                        Some(VirtualKeyCode::Right) => Input::OctaveInc,
+
+                        Some(VirtualKeyCode::Down) => Input::VolumeDec,
+                        Some(VirtualKeyCode::Up) => Input::VolumeInc,
+
+                        Some(VirtualKeyCode::A) => Input::PlayNote(Note::C),
+                        Some(VirtualKeyCode::S) => Input::PlayNote(Note::D),
+                        Some(VirtualKeyCode::D) => Input::PlayNote(Note::E),
+                        Some(VirtualKeyCode::F) => Input::PlayNote(Note::F),
+                        Some(VirtualKeyCode::G) => Input::PlayNote(Note::G),
+                        Some(VirtualKeyCode::H) => Input::PlayNote(Note::A),
+                        Some(VirtualKeyCode::J) => Input::PlayNote(Note::B),
+
+                        _ => return,
                     }
-
-                    KeyCode::Left => Input::OctaveDec,
-                    KeyCode::Right => Input::OctaveInc,
-
-                    KeyCode::Down => Input::VolumeDec,
-                    KeyCode::Up => Input::VolumeInc,
-
-                    KeyCode::Char(c) => {
-                        let note = match c {
-                            'a' => Some(Note::C),
-                            's' => Some(Note::D),
-                            'd' => Some(Note::E),
-                            'f' => Some(Note::F),
-                            'g' => Some(Note::G),
-                            'h' => Some(Note::A),
-                            'j' => Some(Note::B),
-                            _ => None,
-                        };
-
-                        if let Some(note) = note {
-                            Input::PlayNote(note)
-                        } else {
-                            continue;
-                        }
-                    }
-
-                    _ => continue,
-                },
-                _ => continue,
+                }
+                _ => return,
+            },
+            Event::RedrawRequested(_) => {
+                let size = window.inner_size();
+                surface
+                    .resize(
+                        NonZeroU32::new(size.width).unwrap(),
+                        NonZeroU32::new(size.height).unwrap(),
+                    )
+                    .unwrap();
+                let buffer = surface.buffer_mut().unwrap();
+                buffer.present().unwrap();
+                return;
             }
+            _ => return,
         };
 
         match input.send(event) {
             Ok(()) => {}
             Err(SendError(_)) => {
                 // channel is disconnected
-                break;
+                *control_flow = ControlFlow::Exit;
             }
         }
-    }
+    });
 }

160b2b8f09e04b9f403e389e235fc7bbccf53bb7
Hanno Braun <hanno@braun-odw.eu>
2023-07-31 13:25:43 +0200

    Inline function

--- synth/src/ui.rs
+++ synth/src/ui.rs
@@ -8,7 +8,48 @@ pub fn start() -> anyhow::Result<Receiver<Input>> {
 
     thread::spawn(move || {
         loop {
-            let event = read_event();
+            let event = 'event: {
+                if let Event::Key(key) = event::read().unwrap() {
+                    if key.code == KeyCode::Char('c')
+                        && key.modifiers.contains(KeyModifiers::CONTROL)
+                    {
+                        break 'event Some(Input::Quit);
+                    }
+
+                    if key.code == KeyCode::Left {
+                        break 'event Some(Input::OctaveDec);
+                    }
+                    if key.code == KeyCode::Right {
+                        break 'event Some(Input::OctaveInc);
+                    }
+
+                    if key.code == KeyCode::Down {
+                        break 'event Some(Input::VolumeDec);
+                    }
+                    if key.code == KeyCode::Up {
+                        break 'event Some(Input::VolumeInc);
+                    }
+
+                    if let KeyCode::Char(c) = key.code {
+                        let note = match c {
+                            'a' => Some(Note::C),
+                            's' => Some(Note::D),
+                            'd' => Some(Note::E),
+                            'f' => Some(Note::F),
+                            'g' => Some(Note::G),
+                            'h' => Some(Note::A),
+                            'j' => Some(Note::B),
+                            _ => None,
+                        };
+
+                        if let Some(note) = note {
+                            break 'event Some(Input::PlayNote(note));
+                        }
+                    }
+                }
+
+                None
+            };
 
             let event = match event {
                 Some(event) => event,
@@ -26,49 +67,6 @@ pub fn start() -> anyhow::Result<Receiver<Input>> {
     });
 
     Ok(rx)
-}
-
-fn read_event() -> Option<Input> {
-    if let Event::Key(key) = event::read().unwrap() {
-        if key.code == KeyCode::Char('c')
-            && key.modifiers.contains(KeyModifiers::CONTROL)
-        {
-            return Some(Input::Quit);
-        }
-
-        if key.code == KeyCode::Left {
-            return Some(Input::OctaveDec);
-        }
-        if key.code == KeyCode::Right {
-            return Some(Input::OctaveInc);
-        }
-
-        if key.code == KeyCode::Down {
-            return Some(Input::VolumeDec);
-        }
-        if key.code == KeyCode::Up {
-            return Some(Input::VolumeInc);
-        }
-
-        if let KeyCode::Char(c) = key.code {
-            let note = match c {
-                'a' => Some(Note::C),
-                's' => Some(Note::D),
-                'd' => Some(Note::E),
-                'f' => Some(Note::F),
-                'g' => Some(Note::G),
-                'h' => Some(Note::A),
-                'j' => Some(Note::B),
-                _ => None,
-            };
-
-            if let Some(note) = note {
-                return Some(Input::PlayNote(note));
-            }
-        }
-    }
-
-    None
 }
 
 pub enum Input {

db508b12e0214b4aad12799d5a7551fc868602e9
Hanno Braun <hanno@braun-odw.eu>
2023-08-01 21:34:27 +0200

    Remove unused code

--- synth/src/synth/components/scaler.rs
+++ synth/src/synth/components/scaler.rs
@@ -1,6 +1,6 @@
 use crate::synth::{
     clock::Clock,
-    signal::{HasOutput, Output, Signal},
+    signal::{Output, Signal},
 };
 
 use super::SynthComponent;
@@ -19,13 +19,5 @@ impl SynthComponent for Scaler {
             .value(clock)
             .map(|input| input * self.scale.value(clock).unwrap_or(1.));
         self.output.set(output);
-    }
-}
-
-impl HasOutput for Scaler {
-    fn value(&self, clock: &Clock) -> Option<f32> {
-        self.input
-            .value(clock)
-            .map(|input| input * self.scale.value(clock).unwrap_or(1.))
     }
 }

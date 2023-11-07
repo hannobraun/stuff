2011fe96d92acaacdd57c008452df4782d029818
Hanno Braun <hanno@braun-odw.eu>
2023-11-07 19:46:59 +0100

    Refactor

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -1,16 +1,14 @@
 fn main() -> anyhow::Result<()> {
-    eframe::run_simple_native(
-        "Goaler",
-        eframe::NativeOptions::default(),
-        |ctx, frame| {
-            frame.set_maximized(true);
-            ctx.input(|input| {
-                if input.key_pressed(eframe::egui::Key::Escape) {
-                    frame.close();
-                }
-            })
-        },
-    )
+    let config = eframe::NativeOptions::default();
+
+    eframe::run_simple_native("Goaler", config, |ctx, frame| {
+        frame.set_maximized(true);
+        ctx.input(|input| {
+            if input.key_pressed(eframe::egui::Key::Escape) {
+                frame.close();
+            }
+        })
+    })
     .map_err(|err| {
         // `eframe::Error` doesn't implement `Send`, so we need to do a
         // conversion here.

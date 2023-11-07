4aa51916e7b347f072087d52b326bc3c6b1ab035
Hanno Braun <hanno@braun-odw.eu>
2023-11-07 19:39:28 +0100

    Open window

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -1,4 +1,13 @@
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    eframe::run_simple_native(
+        "Goaler",
+        eframe::NativeOptions::default(),
+        |_, _| {},
+    )
+    .map_err(|err| {
+        // `eframe::Error` doesn't implement `Send`, so we need to do a
+        // conversion here.
+        anyhow::anyhow!("{err}")
+    })?;
     Ok(())
 }

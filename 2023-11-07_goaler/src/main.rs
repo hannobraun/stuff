99d982ab2c8db5cb481b8184b3179cf488d44212
Hanno Braun <hanno@braun-odw.eu>
2023-11-07 19:47:52 +0100

    Already start maximized

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -1,10 +1,10 @@
 fn main() -> anyhow::Result<()> {
     let config = eframe::NativeOptions {
+        maximized: true,
         ..eframe::NativeOptions::default()
     };
 
     eframe::run_simple_native("Goaler", config, |ctx, frame| {
-        frame.set_maximized(true);
         ctx.input(|input| {
             if input.key_pressed(eframe::egui::Key::Escape) {
                 frame.close();

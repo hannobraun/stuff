171ac7f093b240a0c7d259a1df6ac7b05998b2d4
Hanno Braun <hanno@braun-odw.eu>
2023-11-07 19:59:13 +0100

    Add more goals

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -15,6 +15,11 @@ fn main() -> anyhow::Result<()> {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             goal(ui, "Goal 1");
+            goal(ui, "Goal 2");
+            goal(ui, "Goal 3");
+            goal(ui, "Goal 4");
+            goal(ui, "Goal 5");
+            goal(ui, "Goal 6");
         });
     })
     .map_err(|err| {

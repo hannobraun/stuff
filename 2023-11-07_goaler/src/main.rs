8062cde2d7a3c53c4eefe9dccb01e3a648b3e5ab
Hanno Braun <hanno@braun-odw.eu>
2023-11-07 20:19:44 +0100

    Refactor

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -9,6 +9,7 @@ fn main() -> anyhow::Result<()> {
     let goals =
         vec!["Goal 1", "Goal 2", "Goal 3", "Goal 4", "Goal 5", "Goal 6"]
             .into_iter()
+            .map(String::from)
             .collect::<Vec<_>>();
 
     eframe::run_simple_native("Goaler", config, move |ctx, frame| {

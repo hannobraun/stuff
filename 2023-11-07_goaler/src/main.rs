62a95f325fe16a7971948f4e1d2ad4ff60fe1dfc
Hanno Braun <hanno@braun-odw.eu>
2023-11-07 19:41:57 +0100

    Maximize window

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -2,7 +2,7 @@ fn main() -> anyhow::Result<()> {
     eframe::run_simple_native(
         "Goaler",
         eframe::NativeOptions::default(),
-        |_, _| {},
+        |_, frame| frame.set_maximized(true),
     )
     .map_err(|err| {
         // `eframe::Error` doesn't implement `Send`, so we need to do a

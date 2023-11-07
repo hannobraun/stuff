fad91f080cadea1d785edae92713197a6b3cab45
Hanno Braun <hanno@braun-odw.eu>
2023-11-07 19:46:19 +0100

    Close on escape

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -2,7 +2,14 @@ fn main() -> anyhow::Result<()> {
     eframe::run_simple_native(
         "Goaler",
         eframe::NativeOptions::default(),
-        |_, frame| frame.set_maximized(true),
+        |ctx, frame| {
+            frame.set_maximized(true);
+            ctx.input(|input| {
+                if input.key_pressed(eframe::egui::Key::Escape) {
+                    frame.close();
+                }
+            })
+        },
     )
     .map_err(|err| {
         // `eframe::Error` doesn't implement `Send`, so we need to do a

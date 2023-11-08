b731c3bf06b7b09cb23b907335a0bd10100f82f2
Hanno Braun <hanno@braun-odw.eu>
2023-11-08 21:14:30 +0100

    Remove initial goals

--- 2023-11-07_goaler/src/main.rs
+++ 2023-11-07_goaler/src/main.rs
@@ -6,11 +6,7 @@ fn main() -> anyhow::Result<()> {
         ..eframe::NativeOptions::default()
     };
 
-    let mut goals =
-        vec!["Goal 1", "Goal 2", "Goal 3", "Goal 4", "Goal 5", "Goal 6"]
-            .into_iter()
-            .map(String::from)
-            .collect::<Vec<_>>();
+    let mut goals = Vec::new();
 
     eframe::run_simple_native("Goaler", config, move |ctx, frame| {
         ctx.input(|input| {

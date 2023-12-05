8436b83d9c7ccc028f65ce43fb08d9ad031e5f69
Hanno Braun <hanno@braun-odw.eu>
2023-12-05 20:22:53 +0100

    Refactor

--- 2023-12-05_synth/src/main.rs
+++ 2023-12-05_synth/src/main.rs
@@ -1,28 +1,7 @@
+mod output;
+
 fn main() -> anyhow::Result<()> {
-    let params = tinyaudio::OutputDeviceParameters {
-        sample_rate: 48000,
-        channels_count: 1,
-        channel_sample_count: 4800,
-    };
-
-    let mut t = 0.;
-
-    let frequency = 220.;
-    let volume = 0.2;
-
-    let _device = tinyaudio::run_output_device(params, move |data| {
-        for samples in data.chunks_mut(params.channels_count) {
-            let value = -1. + t * 2. * volume;
-
-            for sample in samples {
-                *sample = value;
-            }
-
-            t += frequency / params.sample_rate as f32;
-            t %= 1.;
-        }
-    })
-    .map_err(|err| anyhow::anyhow!("Audio error: {err:?}"))?;
+    let _device = output::start();
 
     std::thread::sleep(std::time::Duration::from_secs(1));
 

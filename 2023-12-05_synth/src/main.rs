1927ea9922eb92fb3035a77b45c40bb33e527009
Hanno Braun <hanno@braun-odw.eu>
2023-12-05 20:23:32 +0100

    Fix error handling

--- 2023-12-05_synth/src/main.rs
+++ 2023-12-05_synth/src/main.rs
@@ -1,7 +1,7 @@
 mod output;
 
 fn main() -> anyhow::Result<()> {
-    let _device = output::start();
+    let _device = output::start()?;
 
     std::thread::sleep(std::time::Duration::from_millis(500));
 

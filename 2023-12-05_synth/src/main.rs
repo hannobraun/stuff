725014e979e661451bb2b458ed09e3057ba722a2
Hanno Braun <hanno@braun-odw.eu>
2023-12-05 20:22:58 +0100

    Refactor

--- 2023-12-05_synth/src/main.rs
+++ 2023-12-05_synth/src/main.rs
@@ -3,7 +3,7 @@ mod output;
 fn main() -> anyhow::Result<()> {
     let _device = output::start();
 
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    std::thread::sleep(std::time::Duration::from_millis(1000));
 
     Ok(())
 }

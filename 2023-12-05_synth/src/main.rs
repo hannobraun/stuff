3ef19d167572431f1997e79fa9fbba0c5b749081
Hanno Braun <hanno@braun-odw.eu>
2023-12-06 23:17:49 +0100

    Refactor

--- 2023-12-05_synth/src/main.rs
+++ 2023-12-05_synth/src/main.rs
@@ -1,3 +1,5 @@
+use signal::SAMPLE_RATE;
+
 mod components;
 mod output;
 mod signal;
@@ -5,8 +7,6 @@ mod synth;
 mod wave;
 
 fn main() -> anyhow::Result<()> {
-    const SAMPLE_RATE: u32 = 48000;
-
     let signal = synth::create();
     let _device = output::start::<SAMPLE_RATE>(signal)?;
 
--- 2023-12-05_synth/src/signal/mod.rs
+++ 2023-12-05_synth/src/signal/mod.rs
@@ -2,3 +2,5 @@ mod signal;
 mod source;
 
 pub use self::signal::Signal;
+
+pub const SAMPLE_RATE: u32 = 48000;

a519a305ecdf0294da7c374af16e15e49649fdcf
Hanno Braun <hanno@braun-odw.eu>
2023-12-06 23:23:33 +0100

    Refactor

--- 2023-12-05_synth/src/components/oscillator.rs
+++ 2023-12-05_synth/src/components/oscillator.rs
@@ -1,12 +1,16 @@
 use crate::signal::{Signal, SAMPLE_RATE};
 
-pub fn oscillator(wave: fn(f32) -> f32, frequency: f32) -> Signal {
+pub fn oscillator(
+    wave: fn(f32) -> f32,
+    frequency: impl Into<Signal>,
+) -> Signal {
+    let mut frequency = frequency.into();
     let mut t = 0.;
 
     Signal::from_fn(move || {
         let value = wave(t);
 
-        t += frequency / SAMPLE_RATE as f32;
+        t += frequency.next_value() / SAMPLE_RATE as f32;
         t %= 1.;
 
         value

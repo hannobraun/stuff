9ba530cccf1d922dd20cc953b7cfe688c4b174a7
Hanno Braun <hanno@braun-odw.eu>
2023-12-06 21:00:25 +0100

    Refactor

--- 2023-12-05_synth/src/output.rs
+++ 2023-12-05_synth/src/output.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 pub fn start() -> anyhow::Result<Box<dyn tinyaudio::BaseAudioOutputDevice>> {
     let params = tinyaudio::OutputDeviceParameters {
         sample_rate: 48000,
@@ -5,21 +7,22 @@ pub fn start() -> anyhow::Result<Box<dyn tinyaudio::BaseAudioOutputDevice>> {
         channel_sample_count: 4800,
     };
 
-    let mut t = 0.;
-
     let frequency = 220.;
     let volume = 0.2;
 
+    let mut oscillator =
+        oscillator(sawtooth, frequency, params.sample_rate as f32);
+
     let device = tinyaudio::run_output_device(params, move |data| {
         for samples in data.chunks_mut(params.channels_count) {
-            let value = sawtooth(t) * volume;
+            let Some(value) = oscillator.next() else {
+                return;
+            };
+            let value = value * volume;
 
             for sample in samples {
                 *sample = value;
             }
-
-            t += frequency / params.sample_rate as f32;
-            t %= 1.;
         }
     })
     .map_err(|err| anyhow::anyhow!("Audio error: {err:?}"))?;
@@ -29,4 +32,21 @@ pub fn start() -> anyhow::Result<Box<dyn tinyaudio::BaseAudioOutputDevice>> {
 
 fn sawtooth(t: f32) -> f32 {
     -1. + t * 2.
+}
+
+fn oscillator(
+    wave: fn(f32) -> f32,
+    frequency: f32,
+    sample_rate: f32,
+) -> impl Iterator<Item = f32> {
+    let mut t = 0.;
+
+    iter::from_fn(move || {
+        let value = wave(t);
+
+        t += frequency / sample_rate;
+        t %= 1.;
+
+        Some(value)
+    })
 }

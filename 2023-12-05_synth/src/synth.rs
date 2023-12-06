f569780c018388091772a71dd76379c3fa7a22a0
Hanno Braun <hanno@braun-odw.eu>
2023-12-06 22:30:50 +0100

    Refactor

--- 2023-12-05_synth/src/synth.rs
+++ 2023-12-05_synth/src/synth.rs
@@ -1,11 +1,12 @@
-use crate::{components, signal::Signal, wave};
+use crate::{
+    components::{amplify, oscillator},
+    signal::Signal,
+    wave,
+};
 
 pub fn create<const SAMPLE_RATE: u32>() -> Signal<SAMPLE_RATE> {
     let frequency = 220.;
     let volume = 0.1;
 
-    components::amplify(
-        components::oscillator(wave::sawtooth, frequency),
-        volume,
-    )
+    amplify(oscillator(wave::sawtooth, frequency), volume)
 }

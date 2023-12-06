9fd82fd44967770da6fd67c95c0c01026b7408a7
Hanno Braun <hanno@braun-odw.eu>
2023-12-06 22:00:57 +0100

    Improve formatting

--- 2023-12-05_synth/src/main.rs
+++ 2023-12-05_synth/src/main.rs
@@ -6,6 +6,8 @@ mod wave;
 fn main() -> anyhow::Result<()> {
     const SAMPLE_RATE: u32 = 48000;
     let _device = output::start::<SAMPLE_RATE>()?;
+
     std::thread::sleep(std::time::Duration::from_millis(500));
+
     Ok(())
 }

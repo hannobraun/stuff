bcd1265da2589af7367a6e8b315aa95864066b5d
Hanno Braun <hanno@braun-odw.eu>
2023-12-06 23:36:30 +0100

    Shorten runtime

--- 2023-12-05_synth/src/main.rs
+++ 2023-12-05_synth/src/main.rs
@@ -8,7 +8,7 @@ fn main() -> anyhow::Result<()> {
     let signal = synth::create();
     let _device = output::start(signal)?;
 
-    std::thread::sleep(std::time::Duration::from_millis(500));
+    std::thread::sleep(std::time::Duration::from_millis(200));
 
     Ok(())
 }

36e6f6f9e1755877924ed5f540f89bd375e79553
Hanno Braun <hanno@braun-odw.eu>
2023-12-07 00:16:33 +0100

    Assert audio signals are in expected range

--- 2023-12-05_synth/src/signal/audio.rs
+++ 2023-12-05_synth/src/signal/audio.rs
@@ -3,6 +3,15 @@ pub struct Audio(f32);
 
 impl Audio {
     pub fn new(inner: f32) -> Self {
+        assert!(
+            inner.is_finite(),
+            "`Audio` value must not be NaN or infinite"
+        );
+        assert!(
+            (-1. ..=1.).contains(&inner),
+            "`Audio` value must be within the range of [-1, 1]"
+        );
+
         Self(inner)
     }
 

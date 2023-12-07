8deec6cd2e2ff9c14ead6eb50d0c67e568cbf53f
Hanno Braun <hanno@braun-odw.eu>
2023-12-07 21:43:57 +0100

    Use saturating muliplication in amplifier

--- 2023-12-05_synth/src/components/amplifier.rs
+++ 2023-12-05_synth/src/components/amplifier.rs
@@ -1,4 +1,4 @@
-use crate::signal::{Signal, Value};
+use crate::signal::Signal;
 
 pub trait Amplify {
     fn amplify(self, factor: impl Into<Signal>) -> Signal;
@@ -8,8 +8,6 @@ impl Amplify for Signal {
     fn amplify(mut self, factor: impl Into<Signal>) -> Signal {
         let mut factor = factor.into();
 
-        Signal::from_fn(move || {
-            Value::new(self.next_value().inner() * factor.next_value().inner())
-        })
+        Signal::from_fn(move || self.next_value() * factor.next_value().inner())
     }
 }

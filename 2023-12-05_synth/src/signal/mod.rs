186f07f712da82ed5cc205d8c4ce5e634f308efc
Hanno Braun <hanno@braun-odw.eu>
2023-12-07 15:22:33 +0100

    Remove unused code

--- 2023-12-05_synth/src/signal/mod.rs
+++ 2023-12-05_synth/src/signal/mod.rs
@@ -1,11 +1,8 @@
-mod control;
 mod frequency;
 mod signal;
 mod source;
 mod value;
 
-pub use self::{
-    control::Control, frequency::Frequency, signal::Signal, value::Value,
-};
+pub use self::{frequency::Frequency, signal::Signal, value::Value};
 
 pub const SAMPLE_RATE: u32 = 48000;

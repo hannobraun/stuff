5026f718101883d4985c9e4c8cd959c797893683
Hanno Braun <hanno@braun-odw.eu>
2023-12-07 21:00:41 +0100

    Refactor

--- 2023-12-05_synth/src/signal/value.rs
+++ 2023-12-05_synth/src/signal/value.rs
@@ -17,7 +17,7 @@ impl Value {
         Self { inner: value }
     }
 
-    pub fn from_frequency(frequency: f32, range: &Range) -> Self {
+    pub fn encode_from(frequency: f32, range: &Range) -> Self {
         let value = range.convert_value_to(frequency, &VALUE_RANGE);
         Self::new(value)
     }

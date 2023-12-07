9e627e3825502f13611195a99e71b2b9b7275477
Hanno Braun <hanno@braun-odw.eu>
2023-12-07 15:17:05 +0100

    Update panic messages

--- 2023-12-05_synth/src/signal/audio.rs
+++ 2023-12-05_synth/src/signal/audio.rs
@@ -5,13 +5,10 @@ pub struct Value {
 
 impl Value {
     pub fn new(value: f32) -> Self {
-        assert!(
-            value.is_finite(),
-            "`Audio` value must not be NaN or infinite"
-        );
+        assert!(value.is_finite(), "`Value` must not be NaN or infinite");
         assert!(
             (-1. ..=1.).contains(&value),
-            "`Audio` value must be within the range of [-1, 1]"
+            "`Value` must be within the range of [-1, 1]"
         );
 
         Self { value }

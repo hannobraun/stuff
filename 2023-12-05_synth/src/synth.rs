25b50fe70d2a6386adc49eb61b1ecd7c315dd186
Hanno Braun <hanno@braun-odw.eu>
2023-12-08 20:33:54 +0100

    Refactor

--- 2023-12-05_synth/src/synth.rs
+++ 2023-12-05_synth/src/synth.rs
@@ -5,8 +5,8 @@ use crate::{
 };
 
 pub fn create() -> Signal {
-    let frequency = (220., range::AUDIBLE_RANGE);
+    let frequency = (220., range::AUDIBLE);
     let volume = (0.1, range::AMPLIFIER);
 
-    oscillator(frequency, wave::sawtooth, range::AUDIBLE_RANGE).amplify(volume)
+    oscillator(frequency, wave::sawtooth, range::AUDIBLE).amplify(volume)
 }

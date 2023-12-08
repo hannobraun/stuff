fc0b11b948d5bb83b67fc84b182ab4b78e8d4f0c
Hanno Braun <hanno@braun-odw.eu>
2023-12-08 21:20:24 +0100

    Add square wave

--- 2023-12-05_synth/src/wave.rs
+++ 2023-12-05_synth/src/wave.rs
@@ -5,3 +5,8 @@ pub type Wave = fn(f32) -> Value;
 pub fn sawtooth(t: f32) -> Value {
     Value::new(-1. + t * 2.)
 }
+
+pub fn square(t: f32) -> Value {
+    let value = if t < 0.5 { -1. } else { 1. };
+    Value::new(value)
+}

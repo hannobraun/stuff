1f3e516161e870bbf3b83217203d1cfc0a5a730b
Hanno Braun <hanno@braun-odw.eu>
2023-12-11 20:43:20 +0100

    Adjust tones played

--- 2023-12-05_synth/src/bin/test.rs
+++ 2023-12-05_synth/src/bin/test.rs
@@ -8,15 +8,14 @@ fn main() -> anyhow::Result<()> {
         let frequency = 220.;
         let volume = 0.1;
 
-        let gain =
-            oscillator(20., wave::Square { duty_cycle: 0.5 }, range::LFO)
-                .limit(0., volume, range::AMPLIFIER);
+        let gain = oscillator(8., wave::Square { duty_cycle: 0.4 }, range::LFO)
+            .limit(0., volume, range::AMPLIFIER);
 
         oscillator(frequency, wave::Sawtooth, range::AUDIBLE).amplify(gain)
     };
     let _device = output::start(signal)?;
 
-    std::thread::sleep(std::time::Duration::from_millis(200));
+    std::thread::sleep(std::time::Duration::from_millis(490));
 
     Ok(())
 }

d07c8c4e6aee670b15ff1fdba4cc6a8510a5be37
Hanno Braun <hanno@braun-odw.eu>
2023-12-15 21:22:59 +0100

    Build as UEFI application

--- 2023-12-15_exokernel/src/main.rs
+++ 2023-12-15_exokernel/src/main.rs
@@ -1,6 +1,17 @@
 #![no_std]
 #![no_main]
 
+use uefi::{
+    entry,
+    table::{Boot, SystemTable},
+    Handle, Status,
+};
+
+#[entry]
+fn main(_handle: Handle, _system_table: SystemTable<Boot>) -> Status {
+    Status::SUCCESS
+}
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}

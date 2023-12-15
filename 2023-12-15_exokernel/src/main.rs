c1174444d6550caffd41036edb905eb6597fe419
Hanno Braun <hanno@braun-odw.eu>
2023-12-15 21:54:00 +0100

    Initialize `uefi-services`

--- 2023-12-15_exokernel/src/main.rs
+++ 2023-12-15_exokernel/src/main.rs
@@ -1,8 +1,6 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
 use uefi::{
     entry,
     table::{Boot, SystemTable},
@@ -10,11 +8,7 @@ use uefi::{
 };
 
 #[entry]
-fn main(_handle: Handle, _system_table: SystemTable<Boot>) -> Status {
+fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
     Status::SUCCESS
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
 }

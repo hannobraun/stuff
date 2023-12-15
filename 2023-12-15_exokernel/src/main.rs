ff8a5ef1a0d7edf33e6387e2783b764a46858ae7
Hanno Braun <hanno@braun-odw.eu>
2023-12-15 22:58:21 +0100

    Wait 4 seconds before exiting

--- 2023-12-15_exokernel/src/main.rs
+++ 2023-12-15_exokernel/src/main.rs
@@ -10,5 +10,6 @@ use uefi::{
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
+    system_table.boot_services().stall(4_000_000);
     Status::SUCCESS
 }

61b73a4c0aeec13e9aa2ab5ed23c622b48d7a66a
Hanno Braun <hanno@braun-odw.eu>
2023-12-15 22:59:20 +0100

    Print "Hello, world!"

--- 2023-12-15_exokernel/src/main.rs
+++ 2023-12-15_exokernel/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use log::info;
 use uefi::{
     entry,
     table::{Boot, SystemTable},
@@ -10,6 +11,7 @@ use uefi::{
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
+    info!("Hello, world!");
     system_table.boot_services().stall(4_000_000);
     Status::SUCCESS
 }

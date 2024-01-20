ef753cbda86696f3c1215682a4947ddd8664cae6
Hanno Braun <hanno@braun-odw.eu>
2024-01-20 20:29:51 +0100

    Access response body

--- 2024-01-20_p2p-feed/src/main.rs
+++ 2024-01-20_p2p-feed/src/main.rs
@@ -1,6 +1,9 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let response = reqwest::get("https://hanno.braun-odw.eu/atom.xml").await?;
+    let response = reqwest::get("https://hanno.braun-odw.eu/atom.xml")
+        .await?
+        .text()
+        .await?;
     dbg!(response);
     Ok(())
 }

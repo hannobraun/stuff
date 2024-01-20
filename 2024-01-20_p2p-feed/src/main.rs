3004f9d5cb3d31330f506b08a9c255f4099ec96d
Hanno Braun <hanno@braun-odw.eu>
2024-01-20 20:52:13 +0100

    Update display of entries

--- 2024-01-20_p2p-feed/src/main.rs
+++ 2024-01-20_p2p-feed/src/main.rs
@@ -9,7 +9,27 @@ async fn main() -> anyhow::Result<()> {
     let feed = feed_rs::parser::parse(feed.deref())?;
 
     for entry in feed.entries {
-        dbg!(entry);
+        println!("{}", entry.id);
+
+        let title = entry
+            .title
+            .map(|title| format!("Title: {}", title.content))
+            .unwrap_or_else(|| "no title".to_string());
+
+        println!("- {title}");
+
+        if entry.links.is_empty() {
+            println!("- no links")
+        } else {
+            println!("- Links:");
+
+            for link in entry.links {
+                println!("  - {}", link.href);
+            }
+        }
+
+        println!();
+        println!();
     }
 
     Ok(())

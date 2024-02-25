09cdf5026bcba0017401f8d03a49099adb98971b
Hanno Braun <hanno@braun-odw.eu>
2024-02-25 12:03:08 +0100

    Add utility function

--- 2024-01-20_p2p-feed/src/main.rs
+++ 2024-01-20_p2p-feed/src/main.rs
@@ -66,6 +66,15 @@ impl Item {
     }
 }
 
+pub fn switch<A, B, E>(
+    mut f: impl FnMut(A) -> Result<B, E>,
+) -> impl FnMut(Result<A, E>) -> Result<B, E> {
+    move |res| match res {
+        Ok(a) => f(a),
+        Err(err) => Err(err),
+    }
+}
+
 pub fn track<A, B, E>(
     mut f: impl FnMut(A) -> B,
 ) -> impl FnMut(Result<A, E>) -> Result<B, E> {

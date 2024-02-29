63bc892f2d059b96710a4a59fc30158be91f6977
Hanno Braun <hanno@braun-odw.eu>
2024-02-29 22:59:48 +0100

    Add Zig server

--- 2024-02-29_zig/src/server/main.zig
+++ 2024-02-29_zig/src/server/main.zig
@@ -1,5 +1,27 @@
 const std = @import("std");
+const zap = @import("zap");
 
 pub fn main() !void {
-    std.debug.print("Hello, world!", .{});
+    const currentDir = std.fs.cwd();
+    const publicDir = try currentDir.makeOpenPath("public", .{});
+
+    try currentDir.copyFile("index.html", publicDir, "index.html", .{});
+    try currentDir.copyFile("zig-out/lib/main.wasm", publicDir, "main.wasm", .{});
+
+    var listener = zap.HttpListener.init(.{
+        .port = 8000,
+        .on_request = struct {
+            fn on_request(r: zap.Request) void {
+                r.setStatus(.not_found);
+            }
+        }.on_request,
+        .public_folder = "public",
+        .log = true,
+    });
+    try listener.listen();
+
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
 }

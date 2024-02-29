d529241b65aaea77f03c66b452eabc21e51a32b4
Hanno Braun <hanno@braun-odw.eu>
2024-02-29 12:30:21 +0100

    Configure `zig build` to build WASM module

--- 2024-02-29_zig/build.zig
+++ 2024-02-29_zig/build.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Arch = std.Target.Cpu.Arch;
+const Tag = std.Target.Os.Tag;
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -8,21 +10,22 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = Arch.wasm32, .os_tag = Tag.freestanding } });
 
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig",
+    const lib = b.addSharedLibrary(.{
+        .name = "main",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+    lib.rdynamic = true;
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when

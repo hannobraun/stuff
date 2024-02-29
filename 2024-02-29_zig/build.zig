a81594d3548f6e19df480191990aa0f46c6bb9f8
Hanno Braun <hanno@braun-odw.eu>
2024-02-29 22:16:42 +0100

    Refactor

--- 2024-02-29_zig/build.zig
+++ 2024-02-29_zig/build.zig
@@ -7,22 +7,7 @@ const optimize = std.builtin.Mode.Debug;
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = Target.Cpu.Arch.wasm32, .os_tag = Target.Os.Tag.freestanding } });
-
-    const lib = b.addSharedLibrary(.{
-        .name = "main",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/wasm/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    lib.rdynamic = true;
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    buildAndInstallWasm(b);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -39,4 +24,23 @@ pub fn build(b: *std.Build) void {
     // This will evaluate the `test` step rather than the default, which is "install".
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
+}
+
+fn buildAndInstallWasm(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = Target.Cpu.Arch.wasm32, .os_tag = Target.Os.Tag.freestanding } });
+
+    const lib = b.addSharedLibrary(.{
+        .name = "main",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "src/wasm/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.rdynamic = true;
+
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
 }

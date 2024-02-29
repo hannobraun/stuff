d755ad38646ea8d77a38047de104f488a02a6a0e
Hanno Braun <hanno@braun-odw.eu>
2024-02-29 23:36:44 +0100

    Expose `console.log`

--- 2024-02-29_zig/src/wasm/main.zig
+++ 2024-02-29_zig/src/wasm/main.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
 const testing = std.testing;
 
+extern fn print(ptr: [*]const u8, len: usize) void;
+
 export fn add(a: i32, b: i32) i32 {
+    const message = "Hello, world!";
+    print(message.ptr, message.len);
+
     return a + b;
 }
 
